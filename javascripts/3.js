937142988660b39399a0244fe8a49c98d2a9a942
juanmanueldaza <juanmanueldaza@gmail.com>
2022-07-19 13:06:33 -0300

    disable right click

--- javascripts/3.js
+++ javascripts/3.js
@@ -1,6 +1,7 @@
 function getControls() {
   controls = new THREE.OrbitControls(camera, container);
   controls.enableKeys = false;
+  controls.enablePan = false;
   controls.minDistance = 310;
   controls.maxDistance = 400;
   controls.update();
