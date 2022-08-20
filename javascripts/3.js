e664fd61134ff2d75d97c8a11f7e91ceeed60a96
Federico Rush <rushfederico@gmail.com>
2022-08-20 16:03:23 -0300

    Aca se ve bien el nuevo modelo

--- javascripts/3.js
+++ javascripts/3.js
@@ -2,7 +2,7 @@ function getControls() {
   controls = new THREE.OrbitControls(camera, container);
   controls.enableKeys = false;
   controls.enablePan = false;
-  controls.minDistance = 310;
+  controls.minDistance = 110;
   controls.maxDistance = 400;
   controls.update();
 }
